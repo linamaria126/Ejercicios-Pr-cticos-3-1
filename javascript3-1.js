8e7a1ae8b5c8f77df9094b63f3a84169b421db92
Lina Cardona <linamaria126@gmail.com>
2022-03-23 10:41:19 -0500

    first commit

---
+++ javascript3-1.js
@@ -0,0 +1,97 @@
+//Declarar una variable llamada noValgoNi5 y asignarle el valor 4. Mostrar el valor de la variable por la terminal
+
+var noValgoNi5 = 4
+console.log(noValgoNi5)
+
+/*    Declarar 3 variables:
+
+- miAnioDeNacimiento y asignarle el valor de tu año de nacimiento.
+- meGustariaTener80Anios y asignarle el número 80.
+- voyATener80ElAnio y asignarle el resultado de sumar las 2 variables anteriores
+Mostrar por la terminal el año en el que vas a cumplir 80 años.
+*/
+var miAnioDeNacimiento = 1990
+var meGustariaTener80Anios = 80
+voyATener80ElAnio = miAnioDeNacimiento + meGustariaTener80Anios
+console.log("En el año" + " " + voyATener80ElAnio + " " + "voy a tener 80")
+
+/*
+3.    Declarar 3 variables:
+-variable costoDeUnaTele y asignarle el valor 10000.
+-variable ahorros y asignarle el valor 9000.
+-variable dineroQueMeFalta y asignarle el resultado de restar las 2 variables anteriores.
+Mostrar por la terminal cuanto dinero te falta para comprar la tele.
+*/
+costoDeUnaTele = 10000
+ahorros = 9000
+dineroQueMeFalta = costoDeUnaTele - ahorros
+console.log("El dinero que me falta para comprar la tele es: " + dineroQueMeFalta)
+
+/*
+4.    Declarar 3 variables:
+- dineroQueMeFaltdiasPorAnio y asignarle el número 365
+- cantidadDeAniosEnUnaDecada y asignarle el número 10
+- cantidadDeDiasEnUnaDecada y asignarle el resultado de multiplicar las 2 variables anteriores.
+Mostrar por la terminal cuantos días hay en una década.
+*/
+
+var diasPorAnio = 365
+var cantidadDeAniosEnUnaDecada = 10
+var cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada
+console.log("La cantidad de dias que hay en una década es" + cantidadDeAniosEnUnaDecada)
+
+/*
+5.    Teniendo en cuenta que una pizza siempre tiene 8 porciones, mostrar por la terminal cuantas pizzas tengo en la mesa si conté 24 porciones.
+*/
+var porcionesPizza = 8
+var cantidadPizza = 24/8
+console.log("Para tener 24 porciones de pizza, necesito " + cantidadPizza + " Pizzas")
+
+/*
+6.    Declarar una variable de nombre miAnimalFavorito y asignarle como valor el string que represente el animal que más te gusta. Escribir el código necesario para mostrar por la terminal el valor true si el animal que más te gusta es un 'perro'. De lo contrario, mostrar false. Usar el operador de igualdad (==).
+*/
+var miAnimalFavorito = "León"
+console.log(miAnimalFavorito == "perro")
+/*
+7.    Usar el operador de desigualdad (!=) para mostrar por la terminal el resultado de comparar la cadena de caracteres 'flan' y 'flan con dulce de leche' (Podés crear todas las variables que quieras, incluso podés resolverlo sin usar variables).
+*/
+console.log('flan'!= 'flan con dulce de leche')
+/*
+8.    Declarar una variable llamada soyMayorDeEdad y asignarle el resultado de comparar tu edad y el número 17 usando el operador > (mayor). Mostrar el valor de la variable por la terminal.
+*/
+var soyMayorDeEdad = 32 
+console.log(soyMayorDeEdad > 17)
+/*
+9.    Declarar una variable llamada soyMenorDe25 y asignarle el resultado de comparar tu edad y el número 25 usando el operador < (menor). Mostrar el valor de la variable por la terminal.
+*/
+var  soyMenorDe25 = 32 
+console.log(soyMenorDe25 < 25)
+/*
+10. Declarar una variable llamada estamosEnEnero y asignarle el resultado de comparar si el nombre del mes actual es igual a 'Enero'. Hacerlo usando el operador de igualdad estricta (===). Mostrar el valor de la variable por la terminal.
+*/
+var  estamosEnEnero = 'Enero'
+var mesReal = 'Marzo'
+console.log(estamosEnEnero === mesReal)
+/*
+11. Declarar una variable llamada noEstamosEnEnero y asignarle el resultado de comparar si el nombre del mes actual y el string 'Enero' son diferentes. Hacerlo usando el operador de desigualdad estricta (!==). Mostrar el valor de la variable por la terminal.
+*/
+var noEstamosEnEnero = 'Marzo'
+console.log(noEstamosEnEnero !== 'Enero')
+/*
+12. Declarar 3 variables:
+-miNotaEnElParcial y asignarle un 8
+-notaMinimaParaAprobar y asignarle un 6
+-estoyAprobado y asignarle el resultado de comparar el número las dos variables anteriores usando el operador >=(Mayor o igual).
+Mostrar el valor de la variable estoyAprobado por la terminal.
+*/
+var miNotaEnElParcial = 8
+var notaMinimaParaAprobar = 6
+var estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar
+console.log(estoyAprobado)
+/*
+13. Declarar una variable llamada esHoraDeAlmorzar cuyo valor sea el resultado de comparar con el operador <= (Menor o igual) si la hora actual es menor o igual a 12. Mostrar el valor por la terminal.
+*/
+var horaActual = 13
+var esHoraDeAlmorzar = 12 <= horaActual
+console.log(esHoraDeAlmorzar)
+
